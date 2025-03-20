46de7f8e4a5f9860bb0a4ef3c353170ec4137cdf
Rxphxel223 <raphgaf2901@gmail.com>
2025-03-20 11:52:21 +0100

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -18,7 +18,7 @@ function sendMessage() {
     userMessage.textContent = userInput;
     chatBox.appendChild(userMessage);
 
-    fetch('https://mein-api.up.railway.app/ask', {  // ⚠ HIER deine Railway-URL einfügen
+    fetch('https://chatbot-api-xw3r.onrender.com', { 
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ question: userInput })
